09c4cfa78f314a716c6957f985bc1bfe1931d646
Max <mfong415@gmail.com>
2023-03-17 15:58:47 -0700

    edited bfs

--- src/components/Graphs/utils/algorithims/matrixBFS.tsx
+++ src/components/Graphs/utils/algorithims/matrixBFS.tsx
@@ -1,17 +1,21 @@
 
-const matrixBFS = (matrix:Array<Array<number>>, start : Array<number>): Array<Array<number>> =>{
+const matrixBFS = (matrix:Array<Array<string>>, start : Array<number>): Array<Array<number>> =>{
     const res: Array<number>[] = [];
-    const visited = new Set(); //how do i declare typing on this?
+    const visited = new Set<string>(); //how do i declare typing on this?
 
-    const q: Array<number>[] = [start];
-    const dirs = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-    let node;
+    const q: Array<Array<number>> = [start];
+    const dirs: Array<Array<number>> = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+    let i, j;
     
-    while (q){
-        node = q.shift()
-        visited.add(start)
-        for(let dir of dirs){
-            
+    while (q.length){
+        const node: Array<number> = q.shift()!;
+        visited.add(`${node[0]},${node[1]}`)
+
+    
+        for(const dir of dirs){
+            const i=  node[0] + dir[0];
+            const j = node[1] + dir[1];
+ 
 
         }
 
@@ -24,3 +28,44 @@ const matrixBFS = (matrix:Array<Array<number>>, start : Array<number>): Array<Ar
 }
 
 export default matrixBFS;
+
+
+// interface Point {
+//     row: number;
+//     col: number;
+//   }
+  
+//   function bfs(matrix: number[][], start: Point): void {
+//     const numRows = matrix.length;
+//     const numCols = matrix[0].length;
+//     const visited = new Set<string>();
+  
+//     const queue: Point[] = [start];
+//     visited.add(`${start.row},${start.col}`);
+  
+//     const dirs: Point[] = [
+//       { row: 0, col: 1 },
+//       { row: 1, col: 0 },
+//       { row: 0, col: -1 },
+//       { row: -1, col: 0 },
+//     ];
+  
+//     while (queue.length) {
+//       const curr = queue.shift()!;
+//       const { row, col } = curr;
+//       console.log(`Processing (${row}, ${col})`);
+  
+//       for (const dir of dirs) {
+//         const newRow = row + dir.row;
+//         const newCol = col + dir.col;
+  
+//         if (newRow >= 0 && newRow < numRows && newCol >= 0 && newCol < numCols) {
+//           const key = `${newRow},${newCol}`;
+//           if (!visited.has(key)) {
+//             visited.add(key);
+//             queue.push({ row: newRow, col: newCol });
+//           }
+//         }
+//       }
+//     }
+//   }
