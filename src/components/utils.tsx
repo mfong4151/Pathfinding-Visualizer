f098d3807e30ecf5f732730bdaa3f351eedd35fb
Max <mfong415@gmail.com>
2023-03-19 18:04:33 -0700

    refactored code

--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -1,3 +1,31 @@
-class Node{
+
+class GraphicNode{
+    id: number;
+    type: string;
+    subtype: string;
+    style: string;
+    neighbors: Array<GraphicNode>| undefined;
+    left: GraphicNode| undefined;
+    right: GraphicNode| undefined;
+    prev: GraphicNode| undefined;
+    next: GraphicNode| undefined;
+
+    constructor(id: number, type :string, subtype: string = '', style: string, 
+            neighbors?: GraphicNode[], left?: GraphicNode, right?: GraphicNode, next?: GraphicNode, prev?: GraphicNode
+    ){
+        this.id = id;
+        this.type = type;
+        this.subtype = subtype;
+        this.style = style;
+        this.neighbors = neighbors;
+        this.left = left;
+        this.right = right;
+        this.next = next;
+        this.prev = prev;
+        this.next = next;
     
+    }
+
 }
+
+export default GraphicNode;
