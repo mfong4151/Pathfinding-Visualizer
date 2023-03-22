72b4f6a3013de3d44d8ac7683168b699f83eefae
Max <mfong415@gmail.com>
2023-03-22 09:19:12 -0700

    Made node dragable

--- src/components/generalComponents/DSACanvas/CanvasNode.tsx
+++ src/components/generalComponents/DSACanvas/CanvasNode.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { ItemTypes } from './utils/dragDropConstraints';
+import { useDrag } from 'react-dnd';
 
 interface Props{
     nodeValue: string;
@@ -6,10 +8,27 @@ interface Props{
 
 const CanvasNode: React.FC<Props> = ({nodeValue}) => {
   
+  const [{isDragging}, drag] = useDrag(()=>({
+    type: ItemTypes.CANVAS_NODE,
+    collect: (monitor) =>({
+      isDragging: !!monitor.isDragging()
+    })
+  }))
+
   return (
     
-      <button className='node'>{nodeValue}</button>
-    
+      <button 
+        className='node' 
+        ref={drag}
+        style={{
+          opacity: isDragging ? 0.5 : 1,
+          fontSize: 25,
+          fontWeight: 'bold',
+          cursor: 'move',
+        }}
+      >
+        {nodeValue}
+      </button>
   )
 }
 
---
+++ src/components/generalComponents/DSACanvas/utils/dragDropConstraints.tsx
@@ -0,0 +1,3 @@
+export const ItemTypes = {
+    CANVAS_NODE: 'canvas node'
+}
