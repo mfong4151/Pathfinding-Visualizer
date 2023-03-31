da99365bf0fc3dfa0a850c15e293fa5247c9375e
Max <mfong415@gmail.com>
2023-03-31 15:04:26 -0700

    Added updates section

--- src/components/Splash/index.tsx
+++ src/components/Splash/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './splash.css'
+import { updateSourceFile } from 'typescript';
 
 const aboutThisProject: string =  "This project started off as a side project to help me learn typescript, but gradually became a \
                                     a general tool to help explain the way DSA works. I've personally felt that visual aids are always\
@@ -34,7 +35,12 @@ const Splash:React.FC = () => {
 const RenderUpdates: React.FC = () => {
   return (
     <div>
-      
+      {updates.map((update:string, idx:number) =>
+          <div className='fdr' key={idx}>
+            <h3>{`${update.split(':')[0]}:`} </h3>
+            <p >{`${update.split(':')[1]}`}</p>
+          </div>
+      )}
     </div>
   )
 }
@@ -42,8 +48,32 @@ const RenderUpdates: React.FC = () => {
 const RenderTimeline: React.FC = () => {
   return (
     <div>
-      
+        {timelines.map((status:string, idx:number) =>
+          <div className='fdr' key={idx}>
+            <h3>{`${status.split(':')[0]}:`} </h3>
+            <p >{`${status.split(':')[1]}`}</p>
+          </div>
+      )}
     </div>
   )
 }
 
+
+
+
+
+const updates:string[] = [
+  "4/1 Fix basic styling: Trying to make the page more responsive and just good looking in general. The main style ripped off Leetcode, so it would be great to get the responsive page updating.",
+  "4/3 Fix matrix sizing: I’m going to stick with doing the Matrix for now. I feel like there’s a lot that can be improved, so I want to see that through",
+  "4/5 Add A* and other matrix search algorithims: Pretty straight forward, want to get more algos up.",
+  "4/7 Add Nodes: Add nodes for trees. This will be the groundwork for every other section",
+  "4/17 Add tree algorithim iterators: BFS, and DFS inorder, postorder, preorder",
+  "4/24: Add graph: Back to graphs! I’ll be adding node versions for these as well.",
+  "4/31: Add any styling touchup",
+
+];
+
+const timelines: string[] = [
+
+
+];
