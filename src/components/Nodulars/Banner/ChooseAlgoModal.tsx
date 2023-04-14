e2f6675b2a5aebb711e8387a61ea480eeabbce97
Max <mfong415@gmail.com>
2023-04-14 10:17:21 -0700

    Preparing for beta

--- src/components/Nodulars/Banner/ChooseAlgoModal.tsx
+++ src/components/Nodulars/Banner/ChooseAlgoModal.tsx
@@ -21,7 +21,7 @@ interface MapLocation {
 }
 
 const MAP_LOCATION: MapLocation = {
-  '/matricies':['BFS', 'DFS', 'Best First Search', 'Bidirectional BFS', 'A*'],
+  '/matricies':['BFS', 'DFS', 'Best First Search', 'Bidirectional BFS'],
   '/trees':["Inorder DFS", "Preorder DFS", "Postorder DFS", "BFS"],
 }
 
