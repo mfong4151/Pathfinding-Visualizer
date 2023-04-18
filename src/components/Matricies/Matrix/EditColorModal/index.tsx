1fc8c675808ee58c2b0248fe6b29772483fadae1
Max <mfong415@gmail.com>
2023-04-17 18:47:40 -0700

    Set refs and arguments in editcolormodal

--- src/components/Matricies/Matrix/EditColorModal/index.tsx
+++ src/components/Matricies/Matrix/EditColorModal/index.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 
 interface ModalProps {
     position: { left: number; top: number };
@@ -5,9 +6,20 @@ interface ModalProps {
       editColorModal:boolean;
       setEditColorModal:React.Dispatch<React.SetStateAction<boolean>>
     }
+    color1:string
+    color2:string
+    color3:string
+    color4:string
+    
   }
   
-const EditColorModal: React.FC<ModalProps> = ({ position, colorModalState}) => {
+const EditColorModal: React.FC<ModalProps> = ({ position, colorModalState, color1, color2, color3, color4}) => {
+  const color1Ref = useRef<HTMLButtonElement>(null);
+  const color2Ref = useRef<HTMLButtonElement>(null);
+  const color3Ref = useRef<HTMLButtonElement>(null);
+  const color4Ref = useRef<HTMLButtonElement>(null);
+
+
 
   const handleOnClick = (e:any):void =>{
     e.stopPropagation();
@@ -20,11 +32,23 @@ const EditColorModal: React.FC<ModalProps> = ({ position, colorModalState}) => {
         className="modal"
       >
         <div className="modal-overlay" onClick={() => colorModalState.setEditColorModal(prev => false)}>
-          <div className="modal-content" 
+          <div className="modal-content fdc sb" 
                 style={{ left: position.left, top: position.top, height:`200px`, width:`200px`}}
                 onClick={handleOnClick}
                 >
+                
+                <button className="edit-color-tile" style={{backgroundColor:color1}} ref={color1Ref}>
 
+                </button>
+                <button className="edit-color-tile" style={{backgroundColor:color2}} ref={color2Ref}>
+
+                </button>
+                <button className="edit-color-tile" style={{backgroundColor:color3}} ref={color3Ref}>
+
+                </button>
+                <button className="edit-color-tile" style={{backgroundColor:color4}} ref={color4Ref}>
+
+                </button>
 
             </div>
         </div>
