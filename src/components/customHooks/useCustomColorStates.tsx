836c127c2bfb9aede702e12e22aca27af4d37f65
Max <mfong415@gmail.com>
2023-04-19 06:44:58 -0700

    Refactored color states

--- src/components/customHooks/useCustomColorStates.tsx
+++ src/components/customHooks/useCustomColorStates.tsx
@@ -2,36 +2,47 @@ import React, { useState } from "react"
 import { setBoolean, setString } from "../types/setState"
 
 type states = {
-    startColor: string,
-    setStartColor:  setString,
+    color1: string,
+    color2: string,
+    color3: string,
+    color4: string,
+    setColor1:setString,
+    setColor2:setString,
+    setColor3:setString,
+    setColor4:setString,
     shortestPathColor:string,
     setShortestPathColor: setString,
     editColorModal: boolean,
     setEditColorModal: setBoolean,
-    endColor : string,
-    setEndColor: setString,
     customTransition: boolean,
     setCustomTransition: setBoolean,
 }
 
 const useEditColorStates = (): states  => {
 
-    const [startColor, setStartColor] = useState<string>('')
+    const [color1, setColor1] = useState<string>('')
+    const [color2, setColor2] = useState<string>('')
+    const [color3, setColor3] = useState<string>('')
+    const [color4, setColor4] = useState<string>('')
+
     const [shortestPathColor, setShortestPathColor] = useState<string>('')
     const [editColorModal , setEditColorModal] = useState<boolean>(false);
-    const [endColor, setEndColor] = useState<string>('')
     const [customTransition, setCustomTransition] = useState<boolean>(false)
     
   return {
        
-     startColor, 
-     setStartColor,
+     color1,
+     color2,
+     color3,
+     color4,
+     setColor1, 
+    setColor2, 
+    setColor3, 
+    setColor4, 
      shortestPathColor, 
      setShortestPathColor,
      editColorModal , 
      setEditColorModal,
-     endColor, 
-     setEndColor,
      customTransition,
      setCustomTransition
 
