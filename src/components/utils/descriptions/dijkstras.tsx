97fd590121bbb45e41cb4ceb40d0b8f2be448211
Max <mfong415@gmail.com>
2023-04-22 09:34:17 -0700

    See prev

--- src/components/utils/descriptions/dijkstras.tsx
+++ src/components/utils/descriptions/dijkstras.tsx
@@ -1,3 +1,5 @@
 export const descDijkstras:string[] = [
-    "Dijkstra's Algorithm: This is a shortest path algorithm that finds the shortest path between nodes in a weighted graph. It works by maintaining a list of nodes to visit, and iteratively selecting the node with the smallest known distance from the starting node until the destination node is reached."];
+    "Dijkstra's Algorithm: This is a shortest path algorithm that finds the shortest path between nodes in a weighted graph. It works by maintaining a list of nodes to visit, and iteratively selecting the node with the smallest known distance from the starting node until the destination node is reached."
+
+];
 
