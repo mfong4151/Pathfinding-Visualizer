79d7f4c2ba4d4f7e8edc27037e2ea63609732623
Max <mfong415@gmail.com>
2023-04-25 11:55:11 -0700

    Began refactor of remote

---
+++ src/components/Matricies/MatrixBanner/MatrixRemote/RemoteHtml.tsx
@@ -0,0 +1,51 @@
+import {FC}from 'react'
+import SkipBack from '../../../Nodulars/Banner/Remote/svgs/SkipBack';
+import Play from '../../../Nodulars/Banner/Remote/svgs/Play';
+import Pause from '../../../Nodulars/Banner/Remote/svgs/Pause';
+import FastForward from '../../../Nodulars/Banner/Remote/svgs/Fastforward';
+import SkipForward from '../../../Nodulars/Banner/Remote/svgs/SkipForward';
+
+interface Props{
+  handleOnClick:(e: React.MouseEvent<HTMLButtonElement>) => void;
+  isPlaying: boolean;
+}
+
+const RemoteHtml:FC<Props> = ({handleOnClick, isPlaying}) => {
+    return (
+      <div id='remote' className='fdr se'>
+        <button id='reset' className='remote-btn sq-buttons udc' onClick={handleOnClick}>
+          <SkipBack/>
+        </button>
+    
+        <button id='play' className='remote-btn sq-buttons udc' onClick={handleOnClick}>
+              <Play/>
+          </button> 
+        {
+          !isPlaying 
+            ? <button id='play' className='remote-btn sq-buttons udc' onClick={handleOnClick}>
+              <Play/>
+            </button> 
+    
+            :<button id='pause' className='remote-btn sq-buttons udc' onClick={handleOnClick}>
+              <Pause/>
+            </button>
+          }
+    
+        <button 
+            id='fast-forward' 
+            className='remote-btn sq-buttons udc' 
+            onClick={handleOnClick}
+          >
+    
+          <FastForward/>
+        </button>
+    
+        <button id='skip-forward' className='remote-btn sq-buttons' onClick={handleOnClick}>
+          <SkipForward/>
+        </button>
+      </div>
+    );
+  
+}
+
+export default RemoteHtml
