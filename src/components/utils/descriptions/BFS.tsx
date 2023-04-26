3f8167f6b9c45ca230505256570901e4a46ed7bf
Max <mfong415@gmail.com>
2023-04-26 10:15:33 -0700

    Removed ChatGPT description for bfs

--- src/components/utils/descriptions/BFS.tsx
+++ src/components/utils/descriptions/BFS.tsx
@@ -1,4 +1,8 @@
 
-export const descBFS: string[] = ["Breadth-First Search (BFS): This is a graph traversal algorithm that explores all the vertices of a graph in breadth-first order, i.e., it visits all the vertices at the same level before moving on to the next level. It is often implemented using a queue data structure, and is useful for solving problems such as finding the shortest path between two nodes in an unweighted graph and detecting whether a graph is bipartite."]
+export const descBFS: string[] = [
+    "Breadth-First Search (BFS): This is a graph traversal algorithm that explores all the vertices of a graph in breadth-first order, i.e., it visits all the vertices at the same level before moving on to the next level",
+    "The easiest way to think about BFS is to think of searching in concentric circles: take the inner most one, search it, search its neighbors, search its neighbors' neighbors, etc. until you reach the outermost one.",
+    `Unlike DFS, BFS uses a queue to keep track of positions instead of a stack. Moreover, BFS is usually introduced as the first cannonical "shortest path" algorithm. If you play the algorithm, you will see that it highlights the shortest path from start to finish.`
+    ]
 
 
