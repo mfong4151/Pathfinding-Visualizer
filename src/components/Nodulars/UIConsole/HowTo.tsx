c60edd7563825312c1edc5f8238b0baf018b5cb7
Max <mfong415@gmail.com>
2023-04-28 07:04:57 -0700

    Added to How to section

--- src/components/Nodulars/UIConsole/HowTo.tsx
+++ src/components/Nodulars/UIConsole/HowTo.tsx
@@ -13,10 +13,19 @@ const HowTo: React.FC = () => {
         {`Welcome to the DSA visualizer, you are currently viewing ${location.pathname.slice(1)}.`}
       </p>
       <p>
-        To get started, try dragging and dropping the start and stop icons to the right on to the matrix
+        To get started, try dragging and dropping the start and stop icons to the right on to the matrix.
       </p>
       <p>
-        You can draw and erase walls by clicking on the matrix
+        Optional: You can draw and erase walls by clicking on the matrix.
+      </p>
+      <p>
+        Next, select an algorithm to play with the "Choose your algorithm" dropdown.
+      </p>
+      <p>
+        Next, hit the play button to watch a full automated walkthrough.  
+      </p>
+      <p>
+        Alternative you can hit the fast-forward button to watch a step by step playthrough, or the skip forward button to skip to the end.
       </p>
     </div>
   )
